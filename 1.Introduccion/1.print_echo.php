6ddb0fd0f5235f7582e50f1f00dae7cf07061be6
HumbertoDev <contacto@humbertodg.com>
2021-06-02 08:10:58 -0500

    first commit

---
+++ 1.Introduccion/1.print_echo.php
@@ -0,0 +1,12 @@
+<!-- Lo primero que hice fue instalar xaamp que es como un programa que ma yuda a simular un servidor. Lo bajo en D, entro htdocs y dentro creo el proyecto -->
+
+<!-- PRINT: imprime -->
+<?php 
+print "<h1>Mi Primer Codigo PHP! con print</h1>";
+?>
+<br> <br>
+<!-- ECO : imprime VARIOS resultados, es el mas usado-->
+
+<?php 
+echo "<h3>Mi segundo codigo pero con ECO</h3>", "<br>"," <span style='background-color:black; color: white;'>jeje</span>";
+?>

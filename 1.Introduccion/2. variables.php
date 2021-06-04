e4f8e965631aa28db53a0a591fc427616df9366f
HumbertoDev <contacto@humbertodg.com>
2021-06-03 23:31:42 -0500

    variables

---
+++ 1.Introduccion/2. variables.php
@@ -0,0 +1,14 @@
+<!-- las variables en php se empiezan con $ -->
+<?php 
+
+# siempre se crean con el $ adelante
+$variable1 = 5;
+
+#se puede tener una variable dentro de una cadena de texto SIN concatenar
+## Sin embargo,solo sirve con ""
+echo "Esto es una variable igual a $variable1",'<br><br>';
+# con comillas simples  no funciona ''
+print 'Esto es una variable igual a $variable1';
+
+?>
+
